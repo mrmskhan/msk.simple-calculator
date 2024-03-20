cfd52ca22c5826e045ef9e69177510e0818dc90c
M. SALEEM KHAN <160552274+mrmskhan@users.noreply.github.com>
2024-03-21 03:46:59 +0500

    calculator push to github

---
+++ index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { massege: "enter first number", type: "number", name: "firstnumber" },
+    { massege: "enter second number", type: "number", name: "secondnumber" },
+    {
+        massege: "select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+// cinditional statement
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
